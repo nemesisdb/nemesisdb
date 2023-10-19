5c2575d1dd658e05726305750b020eddc4346e60
FusionCache <ccooper1982@proton.me>
2023-10-19 15:35:53 +0100

    Tests #11

--- test/src/Set.cpp
+++ test/src/Set.cpp
@@ -5,100 +5,36 @@ using namespace fusion::test;
 
 
 
-TEST_F(FusionTest, Scalar)
+TEST_F(FusionTest, SetVarious)
 {
-	const std::vector<TestData> data = 
-	{
-		{TestData { .request = R"({ "KV_SET":{"string1":"asda"}})"_json,	.expected = {R"({ "KV_SET_RSP":{ "st":20, "k":"string1" } })"_json} }},
-		{TestData { .request = R"({ "KV_SET":{"integer1":1}})"_json,			.expected = {R"({ "KV_SET_RSP":{ "st":20, "k":"integer1" } })"_json} }},
-		{TestData { .request = R"({ "KV_SET":{"decimal1":1.5}})"_json,		.expected = {R"({ "KV_SET_RSP":{ "st":20, "k":"decimal1" } })"_json} }}
-	};
-
-
 	TestClient tc;
 
 	ASSERT_TRUE(tc.open());
 
-	for(auto& d : data)
-		tc.test(d);
+	tc.test(TestData {  .request = R"({ "KV_SET":{ "keys":{"myarray":["a", "b"], "mystring":"string", "myinteger":5, "mydecimal":5.5, "myobject":{"user":"toad"}} }})"_json,
+											.expected = {R"({ "KV_SET_RSP":{ "keys":{"myarray":20, "mystring":20, "myinteger":20, "mydecimal":20, "myobject":20} }})"_json}});
+	
 }
 
 
-TEST_F(FusionTest, Object)
+TEST_F(FusionTest, Overwrite)
 {
 	TestClient tc;
 
 	ASSERT_TRUE(tc.open());
 
-	tc.test(TestData { .request = R"({ "KV_SET":{"object1": { "a":"a", "b":2 } }})"_json,	.expected = {R"({ "KV_SET_RSP":{ "st":20, "k":"object1" } })"_json} });
-}
+	tc.test(TestData {  .request = R"({ "KV_SET":{ "keys":{"myarray":["a", "b"], "mystring":"string", "myinteger":5, "mydecimal":5.5, "myobject":{"user":"toad"}} }})"_json,
+											.expected = {R"({ "KV_SET_RSP":{ "keys":{"myarray":20, "mystring":20, "myinteger":20, "mydecimal":20, "myobject":20} }})"_json}});
 
+	tc.test(TestData { 	.request = R"({ "KV_GET":{ "keys":["myarray", "myobject"]} })"_json,
+											.expected = {R"({ "KV_GET_RSP":{ "myarray":["a", "b"], "myobject":{"user":"toad"} }})"_json}});
 
-TEST_F(FusionTest, Array)
-{
-	TestClient tc;
+	tc.test(TestData {  .request = R"({ "KV_SET":{ "keys":{"myarray":["a", "b", "c"], "mystring":"string2", "myinteger":10, "mydecimal":5.521, "myobject":{"user":"toad2"}} }})"_json,
+											.expected = {R"({ "KV_SET_RSP":{ "keys":{"myarray":21, "mystring":21, "myinteger":21, "mydecimal":21, "myobject":21} }})"_json}});
 
-	ASSERT_TRUE(tc.open());
+	tc.test(TestData { 	.request = R"({ "KV_GET":{ "keys":["myarray", "myobject", "mystring", "mydecimal"]} })"_json,
+											.expected = {R"({ "KV_GET_RSP":{ "myarray":["a", "b", "c"], "myobject":{"user":"toad2"}, "mydecimal":5.521, "mystring":"string2" }})"_json}});
 
-	tc.test(TestData { .request = R"({ "KV_SET":{"array1":["a", "b"] } })"_json,	.expected = {R"({ "KV_SET_RSP":{ "st":20, "k":"array1" } })"_json} });
-}
-
-
-TEST_F(FusionTest, MultipleVarious)
-{
-	const std::vector<TestData> data = 
-	{
-		{TestData { .request = R"({ "KV_SET":{"array1":["a", "b"], "array2":[1.5], "myinta":5, "someobject":{"user":"toad"} } })"_json,
-								.expected = {
-															R"({ "KV_SET_RSP":{ "st":20, "k":"array1" } })"_json,
-														 	R"({ "KV_SET_RSP":{ "st":20, "k":"array2" } })"_json,
-															R"({ "KV_SET_RSP":{ "st":20, "k":"myinta" } })"_json,
-															R"({ "KV_SET_RSP":{ "st":20, "k":"someobject"} })"_json
-														}
-							}
-		}
-	};
-
-
-	TestClient tc;
-
-	ASSERT_TRUE(tc.open());
-
-	for(auto& d : data)
-		tc.test(d);
-}
-
-
-TEST_F(FusionTest, Update)
-{
-	TestClient tc;
-
-	ASSERT_TRUE(tc.open());
-
-	tc.test(TestData { .request = R"({ "KV_SET":{"string1":"asda"}})"_json,	.expected = {R"({ "KV_SET_RSP":{ "st":20, "k":"string1" } })"_json} });
-
-	// update
-	tc.test(TestData { .request = R"({ "KV_SET":{"string1":"asda2"}})"_json,	.expected = {R"({ "KV_SET_RSP":{ "st":21, "k":"string1" } })"_json} });
-}
-
-
-TEST_F(FusionTest, KeyShort)
-{
-	TestClient tc;
-
-	ASSERT_TRUE(tc.open());
-
-	tc.test(TestData { .request = R"({ "KV_SET":{"short":["a", "b"] } })"_json,	.expected = {R"({ "KV_SET_RSP":{ "st":25, "k":"short" } })"_json} });
-}
-
-
-TEST_F(FusionTest, IncorrectCommandType)
-{
-	TestClient tc;
-
-	ASSERT_TRUE(tc.open());
-
-	tc.test(TestData { .request = R"({ "KV_SET":[""] })"_json,	.expected = {R"({ "KV_SET_RSP":{ "st":12, "k":"" } })"_json} });
 }
 
 
