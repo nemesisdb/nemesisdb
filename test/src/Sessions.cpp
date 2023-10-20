a91b00c531a8c43bbeb9528b2eff0402a2fa45e8
FusionCache <ccooper1982@proton.me>
2023-10-20 17:25:48 +0100

    session test #11

--- test/src/Sessions.cpp
+++ test/src/Sessions.cpp
@@ -6,85 +6,85 @@ using namespace fusion::test;
 
 // errors
 
-// TEST_F(FusionTest, New_NoName)
-// {
-// 	TestClient tc;
+TEST_F(FusionTest, New_NoName)
+{
+	TestClient tc;
 
-// 	ASSERT_TRUE(tc.openNoSession());
+	ASSERT_TRUE(tc.openNoSession());
 
-// 	tc.test(TestData {  .request =  R"({ "SH_NEW":{ }})"_json,
-// 											.expected = {R"({ "SH_NEW_RSP":{"st":13, "m":"", "tkn":null}})"_json},
-//                       .checkToken = true});
-// }
-
-
-// TEST_F(FusionTest, New_NameEmpty)
-// {
-// 	TestClient tc;
-
-// 	ASSERT_TRUE(tc.openNoSession());
-
-// 	tc.test(TestData {  .request =  R"({ "SH_NEW":{ "name":"" }})"_json,
-// 											.expected = {R"({ "SH_NEW_RSP":{"st":42, "m":"name", "tkn":null}})"_json},
-//                       .checkToken = true});
-// }
+	tc.test(TestData {  .request =  R"({ "SH_NEW":{ }})"_json,
+											.expected = {R"({ "SH_NEW_RSP":{"st":13, "m":"", "tkn":null}})"_json},
+                      .checkToken = true});
+}
 
 
-// TEST_F(FusionTest, New_NameNotString)
-// {
-// 	TestClient tc;
+TEST_F(FusionTest, New_NameEmpty)
+{
+	TestClient tc;
 
-// 	ASSERT_TRUE(tc.openNoSession());
+	ASSERT_TRUE(tc.openNoSession());
 
-// 	tc.test(TestData {  .request =  R"({ "SH_NEW":{ "name":523423 }})"_json,
-// 											.expected = {R"({ "SH_NEW_RSP":{"st":41, "m":"name", "tkn":null}})"_json},
-//                       .checkToken = true});
-// }
-
-
-// TEST_F(FusionTest, New_SharedWrongType)
-// {
-// 	TestClient tc;
-
-// 	ASSERT_TRUE(tc.openNoSession());
-
-// 	tc.test(TestData {  .request =  R"({ "SH_NEW":{ "name":"sesh1", "shared":"a" }})"_json,
-// 											.expected = {R"({ "SH_NEW_RSP":{"st":13, "m":"shared", "tkn":null}})"_json},
-//                       .checkToken = true});
-// }
+	tc.test(TestData {  .request =  R"({ "SH_NEW":{ "name":"" }})"_json,
+											.expected = {R"({ "SH_NEW_RSP":{"st":42, "m":"name", "tkn":null}})"_json},
+                      .checkToken = true});
+}
 
 
-// TEST_F(FusionTest, New_ExpiryWrongType)
-// {
-// 	TestClient tc;
+TEST_F(FusionTest, New_NameNotString)
+{
+	TestClient tc;
 
-// 	ASSERT_TRUE(tc.openNoSession());
+	ASSERT_TRUE(tc.openNoSession());
 
-// 	tc.test(TestData {  .request =  R"({ "SH_NEW":{ "name":"sesh1", "expiry":"aye" }})"_json,
-// 											.expected = {R"({ "SH_NEW_RSP":{"st":13, "m":"expiry", "tkn":null}})"_json},
-//                       .checkToken = true});
-// }
+	tc.test(TestData {  .request =  R"({ "SH_NEW":{ "name":523423 }})"_json,
+											.expected = {R"({ "SH_NEW_RSP":{"st":41, "m":"name", "tkn":null}})"_json},
+                      .checkToken = true});
+}
 
 
-// happy
-// TEST_F(FusionTest, New_SharedNotSet)
-// {
-// 	TestClient tc;
+TEST_F(FusionTest, New_SharedWrongType)
+{
+	TestClient tc;
 
-// 	ASSERT_TRUE(tc.openNoSession());
+	ASSERT_TRUE(tc.openNoSession());
 
-//   TestData sesh1 {  .request =  R"({ "SH_NEW":{ "name":"sesh1" }})"_json,
-// 									  .expected = {R"({ "SH_NEW_RSP":{"st":1, "name":"sesh1"}})"_json}};
+	tc.test(TestData {  .request =  R"({ "SH_NEW":{ "name":"sesh1", "shared":"a" }})"_json,
+											.expected = {R"({ "SH_NEW_RSP":{"st":13, "m":"shared", "tkn":null}})"_json},
+                      .checkToken = true});
+}
 
-//   tc.token = sesh1.token;
 
-//   TestData sesh1Check {   .request =  R"({ "SH_INFO":{ }})"_json,
-// 									        .expected = {R"({ "SH_INFO_RSP":{"st":1, "shared":false, "expiry":{"expires":false, "time":0},"keyCnt":0}})"_json}};
+TEST_F(FusionTest, New_ExpiryWrongType)
+{
+	TestClient tc;
+
+	ASSERT_TRUE(tc.openNoSession());
+
+	tc.test(TestData {  .request =  R"({ "SH_NEW":{ "name":"sesh1", "expiry":"aye" }})"_json,
+											.expected = {R"({ "SH_NEW_RSP":{"st":13, "m":"expiry", "tkn":null}})"_json},
+                      .checkToken = true});
+}
+
+
+//happy
+TEST_F(FusionTest, New_SharedNotSet)
+{
+	TestClient tc;
+
+	ASSERT_TRUE(tc.openNoSession());
+
+  TestData sesh1 {  .request =  R"({ "SH_NEW":{ "name":"sesh1" }})"_json,
+									  .expected = {R"({ "SH_NEW_RSP":{"st":1, "name":"sesh1"}})"_json}};
+
+  tc.token = sesh1.token;
+
+  TestData sesh1Check {   .request =  R"({ "SH_INFO":{ }})"_json,
+									        .expected = {R"({ "SH_INFO_RSP":{"st":1, "shared":false, "expiry":{"expires":false, "time":0},"keyCnt":0}})"_json}};
   
-//   ASSERT_EQ(sesh1.token, sesh1Check.token);
+  ASSERT_EQ(sesh1.token, sesh1Check.token);
 
-// 	tc.test(sesh1);
-// }
+	tc.test(sesh1);
+}
 
 
 TEST_F(FusionTest, New_SharedSet)
