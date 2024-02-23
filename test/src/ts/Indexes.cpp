6a6f7bd1b6384b7ac69d511b11a3ac6afc3b176f
FusionCache <ccooper1982@proton.me>
2024-02-23 16:08:09 +0000

    Complex data test

--- test/src/ts/Indexes.cpp
+++ test/src/ts/Indexes.cpp
@@ -30,7 +30,7 @@ struct MeasureDuration
 
 
 
-TEST_F(TsSeriesTest, Create)
+TEST_F(TsSeriesTest, SimpleData)
 {
   Series s;
   
@@ -66,9 +66,7 @@ TEST_F(TsSeriesTest, Create)
                             )");
 
     auto r = s.get(get, GetRspCmd);
-
-    std::cout << r.rsp << '\n';
-
+    
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 3);
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"], njson::parse(R"([10,11,12])"));
@@ -92,8 +90,6 @@ TEST_F(TsSeriesTest, Create)
                             )");
 
     auto r = s.get(get, GetRspCmd);
-
-    std::cout << r.rsp << '\n';
 
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 2);
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
@@ -119,8 +115,6 @@ TEST_F(TsSeriesTest, Create)
 
     auto r = s.get(get, GetRspCmd);
 
-    std::cout << r.rsp << '\n';
-
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 1);
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"], njson::parse(R"([13])"));
@@ -144,8 +138,6 @@ TEST_F(TsSeriesTest, Create)
                             )");
 
     auto r = s.get(get, GetRspCmd);
-
-    std::cout << r.rsp << '\n';
 
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 2);
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
@@ -171,8 +163,6 @@ TEST_F(TsSeriesTest, Create)
 
     auto r = s.get(get, GetRspCmd);
 
-    std::cout << r.rsp << '\n';
-
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 2);
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"], njson::parse(R"([10,12])"));
@@ -196,8 +186,6 @@ TEST_F(TsSeriesTest, Create)
                             )");
 
     auto r = s.get(get, GetRspCmd);
-
-    std::cout << r.rsp << '\n';
 
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 3);
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
@@ -224,8 +212,6 @@ TEST_F(TsSeriesTest, Create)
 
     auto r = s.get(get, GetRspCmd);
 
-    std::cout << r.rsp << '\n';
-
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 4);
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"], njson::parse(R"([10,11,12,13])"));
@@ -249,8 +235,6 @@ TEST_F(TsSeriesTest, Create)
                             )");
 
     auto r = s.get(get, GetRspCmd);
-
-    std::cout << r.rsp << '\n';
 
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 3);
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
@@ -276,11 +260,61 @@ TEST_F(TsSeriesTest, Create)
 
     auto r = s.get(get, GetRspCmd);
 
-    std::cout << r.rsp << '\n';
-
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 4);
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
     ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"], njson::parse(R"([10,11,12,13])"));
+  }
+}
+
+
+TEST_F(TsSeriesTest, ComplexData)
+{
+  // Similar to SimpleData, but values have more than just 'temp' member
+
+  Series s;
+  
+  
+  ASSERT_EQ(s.create("os1", CreateRspCmd).status, TsRequestStatus::Ok);
+  ASSERT_EQ(s.createIndex("os1", "temp", CreateIndexRspCmd).status, TsRequestStatus::Ok);
+
+  auto add = njson::parse(R"(
+                              {
+                                "ts":"os1",
+                                "t":[10,11,12,13],
+                                "v":[
+                                      {"temp":1, "coords":{"x":2.0, "y":5.0}},
+                                      {"temp":2, "coords":{"x":2.1, "y":5.1}},
+                                      {"temp":1, "coords":{"x":2.2, "y":5.2}},
+                                      {"temp":3, "coords":{"x":2.3, "y":5.3}}
+                                    ]
+                              }
+                            )");
+
+  s.add(add, AddRspCmd);
+
+  
+  // <
+  {
+    auto get = njson::parse(R"(
+                              {
+                                "ts":["os1"],
+                                "rng":[10,13],
+                                "where":
+                                {
+                                  "temp":
+                                  {
+                                    "==":1
+                                  }
+                                }
+                              }
+                            )");
+
+    auto r = s.get(get, GetRspCmd);
+
+    ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), 2);
+    ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"].size(), r.rsp["TS_GET_RSP"]["os1"]["v"].size());
+    ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["t"], njson::parse(R"([10,12])"));
+    ASSERT_EQ(r.rsp["TS_GET_RSP"]["os1"]["v"], njson::parse(R"([{"temp":1, "coords":{"x":2.0, "y":5.0}}, {"temp":1, "coords":{"x":2.2, "y":5.2}}])"));
   }
 }
 
@@ -291,3 +325,4 @@ int main (int argc, char ** argv)
 
 	return RUN_ALL_TESTS();	
 }
+
