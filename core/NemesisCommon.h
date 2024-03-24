104ae5a7ed4b24ae6bb5fdb76c72cebf50cb87f6
FusionCache <ccooper1982@proton.me>
2024-03-24 23:19:29 +0000

    Config version to 3

--- core/NemesisCommon.h
+++ core/NemesisCommon.h
@@ -34,7 +34,7 @@ namespace chrono = std::chrono;
 namespace jsonpath = jsoncons::jsonpath;
 
 static const char * NEMESIS_VERSION = "0.5";
-static const std::size_t NEMESIS_CONFIG_VERSION = 2U;
+static const std::size_t NEMESIS_CONFIG_VERSION = 3U;
 
 static const std::size_t NEMESIS_KV_MINPAYLOAD = 64U;
 static const std::size_t NEMESIS_KV_MAXPAYLOAD = 8U * 1024U;
