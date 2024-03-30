034bb561d5244c2143dc62d72cf916754a29fca2
FusionCache <ccooper1982@proton.me>
2024-03-30 23:45:03 +0000

    Scripted post-build steps

--- docker_build.sh
+++ docker_build.sh
@@ -8,13 +8,19 @@ fi
 
 docker build --target publish -f docker/Dockerfile.alpine -t nemesisdb/nemesisdb:$1 .
 
-# after build:
-#  docker push nemesisdb/nemesisdb:<version>
-#  docker tag nemesisdb/nemesisdb:<version> nemesisdb/nemesisdb:latest
-#  docker push nemesisdb/nemesisdb:latest
+docker push nemesisdb/nemesisdb:$1
 
-# Run with: 
+docker tag nemesisdb/nemesisdb:$1 nemesisdb/nemesisdb:latest
+
+docker push nemesisdb/nemesisdb:latest
+
+
+# Linux run: 
 #   docker run --rm -d --network host --name test1 nemesisdb/nemesisdb:0.3.2
 # 
-# but we first need to change Docker context from the Docker Desktop because it uses a VM, so screws the --network host
+# but first change Docker context from the Docker Desktop with:
+#
 #   docker context use default
+#
+# Docker Desktop uses a VM, so screws the --network host
+#    
