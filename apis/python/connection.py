e96f000b68c2aea9ee93e3f0bffb18d8c772446f
CallumCooper <ccooper1982@proton.me>
2024-11-17 22:31:41 +0000

    not using self.message

--- apis/python/connection.py
+++ apis/python/connection.py
@@ -28,7 +28,6 @@ class Connection:
     self.rspEvt = asio.Event()
     self.connectedSem = asio.Semaphore(0)
     self.rcvTask = None
-    self.queryTask = None
     self.listen_task = asio.create_task(self.open())
     await self.connectedSem.acquire()
 
@@ -41,7 +40,7 @@ class Connection:
         self.ws = websocket
         self.connectedSem.release()
 
-        while True: ## TODO ! uncomment !
+        while True:
           self.rcvTask = asio.create_task(websocket.recv(), name='recv')
           self.message = await self.rcvTask
           self.rspEvt.set()
@@ -53,33 +52,29 @@ class Connection:
       pass
     finally:
       self.rspEvt.clear()
-      
-      if self.queryTask != None:
-        self.queryTask.cancel()
 
       if self.userClosed == False:
         self.rcvTask.cancel()
-      
-      
-  async def query(self, s: dict, expectRsp = True) -> dict:
-    self.queryTask = asio.create_task(self._query(json.dumps(s), expectRsp))   
 
-    # if there is an active query when we are disconnected, the query
-    # task is cancelled, raising an exception.
+
+  async def query(self, s: dict) -> dict:
     try:
-      await self.queryTask
+      queryTask = asio.create_task(self._query(json.dumps(s)))
+      await queryTask
+      msg = queryTask.result()
+      self.rspEvt.clear()
     except asio.CancelledError:
+      # if there is an active query when we are disconnected, the query
+      # task is cancelled, raising an exception
       pass
 
-    return json.loads(self.message) if expectRsp else None
+    return msg
 
 
-  async def _query(self, s: str, expectRsp: bool):
-    await self.ws.send(s, text=True)
-
-    if expectRsp:
-      await self.rspEvt.wait()
-      self.rspEvt.clear()
+  async def _query(self, query: str):
+    await self.ws.send(query, text=True)    
+    await self.rspEvt.wait()
+    return json.loads(self.message)
 
 
   async def close(self):
